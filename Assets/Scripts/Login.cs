947608261631035573de8f532ba4c63d8af64465
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-08-12 16:00:56 +1000

    Adding function

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+
+    public InputField username;
+    public InputField password;
+
+
+    IEnumerator LoginUser(string username, string password)
+    {
+        string createUserURL = "http://localhost/nsirpg/login.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);    
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL,form);
+        yield return webRequest.SendWebRequest();
+        Debug.Log(webRequest.downloadHandler.text);
+    }
+    public void UserLogin()
+    {
+        StartCoroutine(LoginUser(username.text, password.text));
+    }
+}
---
+++ Assets/Scripts/NewUser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NewUser : MonoBehaviour
+{
+
+    public InputField username;
+    public InputField password;
+    public InputField email;
+
+
+    IEnumerator CreateUser(string username, string email, string password)
+    {
+        string createUserURL = "http://localhost/nsirpg/insertUser.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("email", email);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL,form);
+        yield return webRequest.SendWebRequest();
+    }
+    public void CreateNewUser()
+    {
+        StartCoroutine(CreateUser(username.text, email.text, password.text));
+    }
+}
